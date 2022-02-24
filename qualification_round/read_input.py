b124ea415364225f16c7107a3021d829fb747297
akima <akima3104@gmail.com>
2022-02-25 05:02:26 +0900

    input受け取り（まずは人とスキルだけ、プロジェクトは除く）

---
+++ qualification_round/read_input.py
@@ -0,0 +1,82 @@
+
+
+input_dir = "input/a_an_example.in.txt"
+
+def read_input(input_dir):
+
+    #人の名前を入れるリスト
+    contributer_list = []
+    #スキル名を入れるリスト
+    skill_list = []
+    #人とスキルのマトリクスを入れるリスト
+    ability_matrix = []
+
+    input_file = open(input_dir, "r", encoding = "UTF-8")
+    input_data = input_file.readlines()
+    input_length = len(input_data)
+
+    for i in range(input_length):
+        input_data[i] = input_data[i].rstrip("\n")
+        input_data[i] = input_data[i].split(" ")
+
+    ##人のリストとスキルのリストを作る
+    skill_num = 0 
+    contributer_cnt = 0
+    for i in range(input_length):
+        if i == 0:
+            contributer_num = int(input_data[i][0])
+            project_num = int(input_data[i][1])
+
+        else:
+            if skill_num == 0:
+                contributer_list.append(input_data[i][0])
+                skill_num = int(input_data[i][1])
+                skill_cnt = 0
+            else:
+                skill_list.append(input_data[i][0])
+                skill_cnt += 1
+                if skill_cnt == skill_num:
+                    skill_num = 0
+                    contributer_cnt += 1
+
+        if contributer_cnt == contributer_num:
+            break
+
+    skill_list = list(set(skill_list))    
+
+    ##マトリクスを作る
+    for i in range(len(contributer_list)):
+        skill_row = [0]*len(skill_list)
+        ability_matrix.append(skill_row)
+
+    ##
+    skill_num = 0 
+    contributer_cnt = 0
+    for i in range(input_length):
+        if i == 0:
+            continue
+        else:
+            if skill_num == 0:
+                contributer_name = input_data[i][0]
+                contributer_index = contributer_list.index(contributer_name)
+                skill_num = int(input_data[i][1])
+                skill_cnt = 0
+            else:
+                skill_name = input_data[i][0]
+                skill_index = skill_list.index(skill_name)
+                ability_matrix[contributer_index][skill_index] = int(input_data[i][1])
+                skill_cnt += 1
+                if skill_cnt == skill_num:
+                    skill_num = 0
+                    contributer_cnt += 1
+
+        if contributer_cnt == contributer_num:
+            break
+
+    return contributer_list, skill_list, ability_matrix
+
+
+contributer_list, skill_list, ability_matrix = read_input(input_dir)
+print(contributer_list)
+print(skill_list)
+print(ability_matrix)
